3b58a97dd3f8a3592f190d8b8624dd27a6b935e6
DanGO <dannygoerke@icloud,com>
2022-01-31 19:48:31 +0100

    .

--- frontend/frontendvue/src/components/ListField.vue
+++ frontend/frontendvue/src/components/ListField.vue
@@ -147,30 +147,6 @@ export default {
       saveAs(blob, "result.xml");
     }
 
-    function saveExcelFile() {
-      axios
-        .post(
-          config.backendHost + "/excel",
-          {
-            file_name: fileName,
-          },
-          {
-            responseType: "blob",
-          }
-        )
-        .then((response) => {
-          const url = URL.createObjectURL(
-            new Blob([response.data], {
-              type: "application/vnd.ms-excel",
-            })
-          );
-          const link = document.createElement("a");
-          link.href = url;
-          link.setAttribute("download", fileName);
-          document.body.appendChild(link);
-          link.click();
-        });
-    }
     return {
       getAssignmentList,
       saveXMLFile,
